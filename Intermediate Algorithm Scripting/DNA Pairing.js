8b6cf7743071f76cc6191b819c645e7031fe05b0
lays <luckyzuck@126.com>
2016-05-19 18:27:10 +0800

    add dna

---
+++ Intermediate Algorithm Scripting/DNA Pairing.js
@@ -0,0 +1,20 @@
+//接收一段DNA字符串， 返回数组
+// example     pairElement('ATCGA')     return   [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]
+
+function pairElement(str) {
+  	var dnaArr = ['A', 'T', 'G', 'C'];
+  	var reflectArr = ['T', 'A', 'C', 'G'];
+  
+  	var arr = str.split('');
+  	var secArr = [];
+  	for (var i=0; i<arr.length; i++) {
+	  	secArr.push(arr[i].split(''));
+	  	var index = dnaArr.indexOf(secArr[i][0]);
+	  	var another = reflectArr[index];
+	  	secArr[i].push(another);
+  	}
+	console.log(secArr);
+  	return str;
+}
+
+pairElement("GCG");
